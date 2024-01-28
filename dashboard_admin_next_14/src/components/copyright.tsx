16d9626843b8c884a9a6b920a5761c42006195a4
mariocesar33 <mcgato33@hotmail.com>
2024-01-28 12:15:53 -0100

    copyright

---
+++ dashboard_admin_next_14/src/components/copyright.tsx
@@ -0,0 +1,7 @@
+export function Copyright() {
+  return (
+    <footer className="text-xs text-gray-500">
+      Mário César &copy; - {new Date().getFullYear()}
+    </footer>
+  )
+}
--- dashboard_admin_next_14/src/components/sidebar/index.tsx
+++ dashboard_admin_next_14/src/components/sidebar/index.tsx
@@ -11,43 +11,56 @@ import {
   UsersRound,
 } from 'lucide-react'
 
+import { Copyright } from '../copyright'
 import { Button } from '../ui/button'
 import { NavItem } from './nave-item'
 import { Profile } from './profile'
 
 export function Sidebar() {
   return (
-    <aside className="w-full space-y-4">
+    <aside className="flex w-full flex-col justify-between">
       <div className="">
         <Profile />
       </div>
 
-      <nav className=" w-full space-y-1">
-        <NavItem href="/dashboard" title="Dashboard" icon={LayoutDashboard} />
-        <NavItem href="/dashboard/users" title="Users" icon={CircleUserRound} />
-        <NavItem
-          href="/dashboard/products"
-          title="Products"
-          icon={ShoppingBag}
-        />
-        <NavItem
-          href="/dashboard/transactions"
-          title="Transactions"
-          icon={DollarSign}
-        />
+      <div>
+        <nav className="w-full space-y-1">
+          <NavItem href="/dashboard" title="Dashboard" icon={LayoutDashboard} />
+          <NavItem
+            href="/dashboard/users"
+            title="Users"
+            icon={CircleUserRound}
+          />
+          <NavItem
+            href="/dashboard/products"
+            title="Products"
+            icon={ShoppingBag}
+          />
+          <NavItem
+            href="/dashboard/transactions"
+            title="Transactions"
+            icon={DollarSign}
+          />
 
-        <NavItem href="/dashboard/revenue" title="Revenue" icon={Briefcase} />
-        <NavItem href="/dashboard/reports" title="Reports" icon={LineChart} />
-        <NavItem href="/dashboard/teams" title="Teams" icon={UsersRound} />
+          <NavItem href="/dashboard/revenue" title="Revenue" icon={Briefcase} />
+          <NavItem href="/dashboard/reports" title="Reports" icon={LineChart} />
+          <NavItem href="/dashboard/teams" title="Teams" icon={UsersRound} />
 
-        <NavItem href="/dashboard/settings" title="Settings" icon={Settings} />
-        <NavItem href="/dashboard/help" title="Help" icon={HelpCircle} />
-      </nav>
+          <NavItem
+            href="/dashboard/settings"
+            title="Settings"
+            icon={Settings}
+          />
+          <NavItem href="/dashboard/help" title="Help" icon={HelpCircle} />
+        </nav>
 
-      <Button className="flex w-full items-center justify-start gap-2 bg-transparent text-white hover:bg-slate-600">
-        <LogOut className="h-5 w-5" />
-        Logout
-      </Button>
+        <Button className="flex w-full items-center justify-start gap-2 bg-transparent text-white hover:bg-slate-600">
+          <LogOut className="h-5 w-5" />
+          Logout
+        </Button>
+      </div>
+
+      <Copyright />
     </aside>
   )
 }
