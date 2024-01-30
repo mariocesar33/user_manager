98d94f2878b023f731e10e45702f9eedbf7bb005
mariocesar33 <mcgato33@hotmail.com>
2024-01-30 12:12:21 -0100

    página login

--- dashboard_admin_next_14/src/app/(auth)/sign-in/page.tsx
+++ dashboard_admin_next_14/src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,27 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
 export default function SignIn() {
-  return <h1>Sign-In</h1>
+  return (
+    <div className="flex h-screen w-screen">
+      <form
+        className="m-auto flex flex-col items-center justify-center space-y-4 rounded-sm bg-muted px-7 py-10"
+        action=""
+      >
+        <h1 className="text-xl font-bold">Login</h1>
+        <Input
+          className="border border-muted-foreground"
+          type="text"
+          placeholder="username"
+        />
+        <Input
+          className="border border-muted-foreground"
+          type="password"
+          placeholder="password"
+        />
+
+        <Button className="w-full font-semibold">Login</Button>
+      </form>
+    </div>
+  )
 }
